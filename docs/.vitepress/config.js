48fde33865fdb11a76f7f32f3a3a01814153b198
Errol <errol.green@pulsewave.io>
2025-06-25 17:55:56 -0400

    bump docs version

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     }
   },
   themeConfig: {
-    appVersion: '0.2.0',
+    appVersion: '0.2.1',
     github: {
       releasesUrl: 'https://github.com/errolgr/pd2-trade/releases'
     },
