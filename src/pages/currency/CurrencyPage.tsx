294fa314524ad2f5aed11533224006738bd6270e
synpoox <exozionn@gmail.com>
2025-07-02 20:18:33 -0700

    remove unnecessary DialogueProvider

--- src/pages/currency/CurrencyPage.tsx
+++ src/pages/currency/CurrencyPage.tsx
@@ -2,16 +2,13 @@ import * as React from 'react';
 import { OptionsProvider } from '@/hooks/useOptions';
 import { Pd2WebsiteProvider } from '@/hooks/pd2website/usePD2Website';
 import { CurrencyValuation } from './CurrencyValuation';
-import { DialogProvider } from '@/hooks/useDialog';
 
 export function CurrencyPage() {
   return (
-    <DialogProvider>
       <OptionsProvider>
         <Pd2WebsiteProvider>
           <CurrencyValuation />
         </Pd2WebsiteProvider>
       </OptionsProvider>
-    </DialogProvider>
   );
 }
