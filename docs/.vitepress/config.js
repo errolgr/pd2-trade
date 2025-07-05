46f628fd1a03babf46afd078e6370b71a3c4fa4e
errolgr <errol_gr@hotmail.com>
2025-07-05 00:54:28 -0700

    version bump docs: 0.3.0

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     }
   },
   themeConfig: {
-    appVersion: '0.2.10',
+    appVersion: '0.3.0',
     github: {
       releasesUrl: 'https://github.com/errolgr/pd2-trade/releases'
     },
